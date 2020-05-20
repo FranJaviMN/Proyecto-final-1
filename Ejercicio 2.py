09d89f745f3032992c8938b2effa19e96ab8cc1a
Francisco Javier Martín Núñez <franjaviermn17100@gmail.com>
2020-05-20 14:08:53 +0200

    Añadida la parte final del ejercicio.

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -7,3 +7,22 @@ key=os.environ["clave_propia"]
 #payload = {"locale=es_ES":"es_ES"}
 headers={"Authorization":"bearer "+key}
 URL_BASE_CARTAS = "https://us.api.blizzard.com/"
+
+#En esta parte le vamos a pedir al usuario que nos de que palabra o personaje quiere buscar en la api.
+
+palabra_clave=str(input('Dime que palabra o personaje quieres buscar: '))
+
+peticion_cartas=requests.get(URL_BASE_CARTAS+"hearthstone/cards?locale=es_ES&textFilter="+palabra_clave,headers=headers)
+
+if peticion_cartas.status_code == 200:
+    lista_coincidencias=[]
+    doc_cartas=peticion_cartas.json()
+    while doc_cartas['cardCount'] == 0:
+        print('No hay coincidencias, intentela de nuevo')
+        palabra_clave=str(input('Dime que palabra o personaje quieres buscar: '))
+        peticion_cartas=requests.get(URL_BASE_CARTAS+"hearthstone/cards?locale=es_ES&textFilter="+palabra_clave,headers=headers)
+        doc_cartas=peticion_cartas.json()
+    for cartas in doc_cartas['cards']:
+        print(cartas['name']+' Imagen: '+cartas['image'])
+else:
+    print('Error, no se ha podido obtener la informacion')
